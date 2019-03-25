0c30f58120e82b1640c9fe0905a20690317870a5
win84814 <bb131517@gmail.com>
2019-03-25 23:38:22 +0800

    Test button

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
 
-root = Tk()
-app = Frame(root)
-hello = Label(root, text="Hello Tk!", width="30", height="5")
-hello.pack()
+#ttk 是 tkinter 另外一組視窗元件的程式庫， ttk 可替視窗元件設定不同的樣式。
+class Application(ttk.Frame):
+    def __init__(self, master):
+        ttk.Frame.__init__(self, master)
+        self.pack()
+
+        self.button = ttk.Button(self)
+        self.button["text"] = "Click Me!"
+        self.button["command"] = self.popup_hello
+        self.button.pack()
+        
+
+    def popup_hello(self):
+        showinfo("Hello", "Hello Tk!")
+
+root = tk.Tk()
+app = Application(root)
 root.mainloop()
+
