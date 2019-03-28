e5c37c993e44e27e0bde5437a61a0dec530ab12d
win84814 <bb131517@gmail.com>
2019-03-28 16:08:05 +0800

    Full window size

--- main.py
+++ main.py
@@ -7,17 +7,35 @@ class Application(ttk.Frame):
     def __init__(self, master):
         ttk.Frame.__init__(self, master)
         self.pack()
+        
+        root.title("Gaze Block Estimation")
+        root.resizable(False,False)
 
+        self.set_window_size()
+
+    def add_button(self):
         self.button = ttk.Button(self)
         self.button["text"] = "Click Me!"
         self.button["command"] = self.popup_hello
         self.button.pack()
-        
+
+    def set_window_size(self):
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        root.geometry('%sx%s+%s+%s' % (screen_width, screen_height, 0, 0) )   #center window on desktop
 
     def popup_hello(self):
-        showinfo("Hello", "Hello Tk!")
+        showinfo("Hello", ">_<")
+    
+    def add_canvas(self):
+        cv = tk.Canvas(root,width=200,height=200,bg = 'white')
+        cv.create_rectangle(10,10,20,20)
+        cv.create_rectangle(30,30,40,40)
+        cv.pack()
+
 
 root = tk.Tk()
 app = Application(root)
+#app.add_canvas()
 root.mainloop()
 
