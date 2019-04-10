93ab155faeb849a818f30d750ad2661cde759f00
win84814 <bb131517@gmail.com>
2019-04-10 13:47:00 +0800

    Connection completed

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from PIL import ImageTk
 import PIL.Image
 import cv2
 from tkinter import Frame, Label, Button, YES, LEFT, BOTH
-from tkinter.messagebox import showinfo
+from tkinter.messagebox import showinfo, askyesno
+import threading
 
 
 class Application(Frame):
@@ -27,27 +28,34 @@ class Application(Frame):
                 self.nine_grid[row][column].grid(row=row, column=column)
         frame1.pack(side=LEFT, fill=BOTH, expand=YES)
 
+        self.nine_grid[1][1].bind("<Button-1>", self.popup_hello_event)
+
         # init right grid (for control buttons and webcam)
         frame2 = Frame(master)
         self.connect_to_camera_btn = Button(frame2, text='Connect')
-        self.connect_to_camera_btn.bind("<Button-1>", open_camera)
+        self.connect_to_camera_btn.bind("<Button-1>", self.open_camera)
         self.connect_to_camera_btn.pack()
         self.status = Label(frame2, text="Can't find camera")
         self.status.pack()
         self.predict = Label(frame2, text="Predict : center")
         self.predict.pack()
 
+        # init webcam logo
         image = cv2.imread(r'D:\DL\code\Gaze-Block-Estimation\webcam.jpg')
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = PIL.Image.fromarray(image)
         image = ImageTk.PhotoImage(image)
-
         self.webcam = Label(frame2, image=image)
         self.webcam.image = image
         self.webcam.place(width=200, height=200)
         self.webcam.pack()
         frame2.pack(side=LEFT, padx=10)
         
+        # init webcam object & thread
+        self.thread_w = threading.Thread(target=self.webcam_thread)
+        self.thread_w_signal = False
+        self.capture = cv2.VideoCapture(0)
+
         #self.set_window_size()
 
     def set_window_size(self):
@@ -61,25 +69,34 @@ class Application(Frame):
     def popup_hello_event(self, event):  # for bind
         showinfo("Hello", ">_<")
     
-    def open_camera(self):
-        capture = cv2.VideoCapture(0)
+    def open_camera(self, event):
+        self.thread_w_signal = True
+        self.thread_w.start()
+    
+    def webcam_thread(self):
 
         # 設定影像的尺寸大小
-        capture.set(cv2.CAP_PROP_FRAME_WIDTH, 200)
-        capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 200)
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 200)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 200)
 
-        while(True):
-            ret, frame = capture.read()
-            #cv2.imshow('frame', frame)
-            self.webcam.image = frame
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        while(self.thread_w_signal):
+            ret, frame = self.capture.read()
+            tkframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            tkframe = PIL.Image.fromarray(tkframe)
+            tkframe = ImageTk.PhotoImage(tkframe)
+            self.webcam.imgtk = tkframe
+            self.webcam.config(image=tkframe)
 
-        capture.release()
-        cv2.destroyAllWindows()
-
+    def on_closing(self):
+        ans = askyesno(title='Quit', message='Do you want to quit?')
+        if ans:
+            self.thread_w_signal = False
+            root.destroy()
+        else:
+            return
 
 root = tk.Tk()
 app = Application(root)
+root.protocol("WM_DELETE_WINDOW", app.on_closing)
 root.mainloop()
 
