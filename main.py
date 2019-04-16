eda68b1ba52b2656f7b6b4c14e8eb5fc6f90f78b
win84814 <bb131517@gmail.com>
2019-04-16 13:16:00 +0800

    Predict with webcam

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import cv2
 from tkinter import Frame, Label, Button, YES, LEFT, BOTH
 from tkinter.messagebox import showinfo, askyesno
 import threading
+import dlib_utils
+
+import numpy as np
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.models import load_model
+from tensorflow.python.keras.preprocessing import image
 
 
 class Application(Frame):
@@ -28,16 +34,19 @@ class Application(Frame):
                 self.nine_grid[row][column].grid(row=row, column=column)
         frame1.pack(side=LEFT, fill=BOTH, expand=YES)
 
-        self.nine_grid[1][1].bind("<Button-1>", self.popup_hello_event)
-
         # init right grid (for control buttons and webcam)
         frame2 = Frame(master)
         self.connect_to_camera_btn = Button(frame2, text='Connect')
         self.connect_to_camera_btn.bind("<Button-1>", self.open_camera)
         self.connect_to_camera_btn.pack()
+
+        self.load_model_btn = Button(frame2, text='Load model')
+        self.load_model_btn.bind("<Button-1>", self.load_model)
+        self.load_model_btn.pack()
+
         self.status = Label(frame2, text="Can't find camera")
         self.status.pack()
-        self.predict = Label(frame2, text="Predict : center")
+        self.predict = Label(frame2, text="No model")
         self.predict.pack()
 
         # init webcam logo
@@ -56,6 +65,10 @@ class Application(Frame):
         self.thread_w_signal = False
         self.capture = cv2.VideoCapture(0)
 
+        # init model thread
+        self.thread_m = threading.Thread(target=self.load_model_thread)
+        self.model = None
+
         #self.set_window_size()
 
     def set_window_size(self):
@@ -68,25 +81,67 @@ class Application(Frame):
 
     def popup_hello_event(self, event):  # for bind
         showinfo("Hello", ">_<")
-    
+
+    def popup_qq_event(self):
+        showinfo("QQ", "QAQ")
+
     def open_camera(self, event):
         self.thread_w_signal = True
         self.thread_w.start()
+        self.status['text'] = 'Using webcam'
     
+    def load_model(self, event):
+        self.thread_m.start()
+
+    def load_model_thread(self):
+        print('loading model...')
+        model_path = r'D:\DL\model\two_eyes_gaze_keras_resnet50_model.h5'
+        self.model = load_model(model_path)
+        
+        self.model.predict(np.zeros((1, 32, 128, 3)))  # before using model, must predict once to avoid ' Tensor is not an element of this graph.'
+
+        self.predict['text'] = 'Using model'
+        print('loading completed!')
+
     def webcam_thread(self):
+        cls_list = ['LeftUp', 'Up', 'RightUp',
+                    'Left', 'Center', 'Right',
+                    'LeftDown', 'Down', 'RightDown']
 
         # 設定影像的尺寸大小
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 200)
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 200)
 
+        # 刷新縮圖
         while(self.thread_w_signal):
-            ret, frame = self.capture.read()
-            tkframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            ret, frame = self.capture.read()  # frame is BGR
+            tkframe = dlib_utils.find_face_and_eyes(frame)
+            tkframe = cv2.cvtColor(tkframe, cv2.COLOR_BGR2RGB)
             tkframe = PIL.Image.fromarray(tkframe)
             tkframe = ImageTk.PhotoImage(tkframe)
             self.webcam.imgtk = tkframe
             self.webcam.config(image=tkframe)
 
+            
+            # predict
+            if self.model is not None:
+                catch, two_eyes = dlib_utils.crop_two_eyes(frame) 
+                # avoid crop nothing
+                if catch:
+                    
+                    #two_eyes = cv2.resize(two_eyes, (128, 32))
+                    #two_eyes = image.load_img(two_eyes, target_size=(32, 128)) # keras is RGB
+                    #two_eyes = two_eyes[..., ::-1]  # transfer BGR to RGB
+                    two_eyes = cv2.cvtColor(two_eyes, cv2.COLOR_BGR2RGB)
+                    x = image.img_to_array(two_eyes)
+                    x = np.expand_dims(x, axis=0)  # shape(1, 32, 128, 3)
+                    x /= 255  # normalize
+                    pred = self.model.predict(x)[0]
+                    top_inds = pred.argsort()[::-1][:5]
+                    ans = str(cls_list[top_inds[0]]) + ' : ' + str(pred[top_inds[0]])
+                    self.predict['text'] = ans
+                    print(ans)
+            
     def on_closing(self):
         ans = askyesno(title='Quit', message='Do you want to quit?')
         if ans:
