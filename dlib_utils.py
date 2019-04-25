72e8ad55a01bf00464c4ca95191c23915fd01602
win84814 <bb131517@gmail.com>
2019-04-25 16:18:48 +0800

    Fix frame_queue for all are 0

--- dlib_utils.py
+++ dlib_utils.py
@@ -34,7 +34,7 @@ def find_face_and_eyes(img):
             # two eyes
             tx, ty, tw, th = get_rectangle(shape[36:47, :])
             
-            cv2.rectangle(img, 
+            cv2.rectangle(img,
                           (tx-10, ty-10),
                           (tx+tw+10, ty+th+10),
                           (255, 0, 0),
@@ -43,7 +43,7 @@ def find_face_and_eyes(img):
     return img
 
 
-def crop_two_eyes(img):
+def crop_two_eyes(img, resize_width=128, resize_height=32):
     img = imutils.resize(img)  # 調整圖片寬度
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     rects = detector(gray, 1)
@@ -52,50 +52,27 @@ def crop_two_eyes(img):
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
         (x, y, w, h) = face_utils.rect_to_bb(rect)
-        #cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
         
         for i, (x, y) in enumerate(shape):
             # two eyes
             tx, ty, tw, th = get_rectangle(shape[36:47, :])
-            '''
-            cv2.rectangle(img, 
-                          (tx-10, ty-10),
-                          (tx+tw+10, ty+th+10),
-                          (255, 0, 0),
-                          1)
-            '''
-    #return img[tx-10:tx+tw+10, ty-10:ty+th+10]
-    #print(tx, ty, tw, th)
+
     if tx == 0 or ty == 0:
         return False, img
     else:
-        #return img[tx-10:tx+tw+10, ty-10:ty+th+10]
-        new_img = img[max(ty-10,0):ty+th+10, max(tx-10,0):tx+tw+10]
-        new_img = cv2.resize(new_img, (128,32))
-        return True, new_img #img[ty-10:ty+th+10, tx-10:tx+tw+10]
+        # return img[tx-10:tx+tw+10, ty-10:ty+th+10]
+        new_img = img[max(ty-10, 0):ty+th+10, max(tx-10, 0):tx+tw+10]
+        new_img = cv2.resize(new_img, (resize_width, resize_height))
+        cv2.imwrite(r'D:\DL\code\Gaze-Block-Estimation\my_eyes2.png', new_img)
+        return True, new_img  # img[ty-10:ty+th+10, tx-10:tx+tw+10]
 
 
 def main():
-    '''
-    image_path = 'D:/DL/dataset/columbia_gaze_data_set/Columbia Gaze Data Set/0006/0006_2m_0P_0V_0H.jpg'
-    #image_path = r'D:\DL\dataset\MPIIGaze\Data\Original\p00\day13\0203.jpg'
-    image = cv2.imread(image_path)#輸入圖片實參則讀入圖片
-
-    # show the face number 人臉序號的標記（可識別多張）
-    #cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
-    #            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    #image = find_face_and_eyes(image)
-    image = find_face_and_eyes(image)
-    cv2.imshow("Output", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
-    '''
     cap = cv2.VideoCapture(0)
     while(cap.isOpened()):
         ret, frame = cap.read()
         catch, frame = crop_two_eyes(frame) 
-        cv2.imwrite(r'D:\DL\code\Gaze-Block-Estimation\my_eyes.png',frame)
+        #cv2.imwrite(r'D:\DL\code\Gaze-Block-Estimation\my_eyes.png', frame)
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import tkinter as tk
-from PIL import ImageTk
-import PIL.Image
+from PIL import ImageTk, Image
 import cv2
 from tkinter import Frame, Label, Button, YES, LEFT, BOTH
 from tkinter.messagebox import showinfo, askyesno
 import threading
 import dlib_utils
-
 import numpy as np
 from keras import backend as K
 from keras.models import model_from_json
 from keras.preprocessing import image
+
+img_width, img_height = 128, 32
 
 
 class Application(Frame):
@@ -53,7 +53,7 @@ class Application(Frame):
         # init webcam logo
         image = cv2.imread(r'D:\DL\code\Gaze-Block-Estimation\webcam.jpg')
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = PIL.Image.fromarray(image)
+        image = Image.fromarray(image)
         image = ImageTk.PhotoImage(image)
         self.webcam = Label(frame2, image=image)
         self.webcam.image = image
@@ -72,10 +72,10 @@ class Application(Frame):
 
         # init gaze direction thread
         self.thread_g = threading.Thread(target=self.gaze_direction_thread)
-        self.frame_queue = np.empty((0,128,32,3))
+        self.frame_queue = np.empty((0, img_height, img_width, 3))
 
         #self.set_window_size()
-
+        
     def set_window_size(self):
         screen_width = root.winfo_screenwidth() - 300
         screen_height = root.winfo_screenheight() - 300
@@ -111,14 +111,13 @@ class Application(Frame):
 
     def load_model_thread(self):
         print('loading model...')
-        #model_path = r'D:\DL\model\two_eyes_gaze_keras_resnet50_model.h5'
-        model_path = r'D:\DL\model\20190419\two_eyes_gaze_keras_resnet50_model.json'
-        model_weight_path = r'D:\DL\model\20190419\two_eyes_gaze_keras_resnet50_model.h5'
+        model_path = r'D:\DL\model\20190424\two_eyes_gaze_keras_resnet50_model.json'
+        model_weight_path = r'D:\DL\model\20190424\two_eyes_gaze_keras_resnet50_model.h5'
         with open(model_path, 'r') as file:
             model_json = file.read()
             self.model = model_from_json(model_json)
         self.model.load_weights(model_weight_path)
-        self.model.predict(np.zeros((1, 128, 32, 3)))  # before using model, must predict once to avoid 'Tensor is not an element of this graph.'
+        self.model.predict(np.zeros((1, img_height, img_width, 3)))  # before using model, must predict once to avoid 'Tensor is not an element of this graph.'
         self.predict['text'] = 'Using model'
         print('loading completed!')
 
@@ -128,33 +127,31 @@ class Application(Frame):
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-        
+
         # 刷新縮圖
         while(self.thread_w_signal):
             ret, frame = self.capture.read()  # frame is BGR
             tkframe = dlib_utils.find_face_and_eyes(frame)
             tkframe = cv2.cvtColor(tkframe, cv2.COLOR_BGR2RGB)  # transfer BGR to RGB
-            tkframe = PIL.Image.fromarray(tkframe)
+            tkframe = Image.fromarray(tkframe)
             tkframe = ImageTk.PhotoImage(tkframe)
             self.webcam.imgtk = tkframe
             self.webcam.config(image=tkframe)
 
             # predict
             if self.model is not None:
-                catch, two_eyes = dlib_utils.crop_two_eyes(frame) 
+                catch, two_eyes = dlib_utils.crop_two_eyes(frame, img_width, img_height) 
                 
-                self.reset_nine_grid_color()
                 # avoid crop nothing
                 if catch:
                     two_eyes = cv2.cvtColor(two_eyes, cv2.COLOR_BGR2RGB)  # transfer BGR to RGB
-                    x = image.img_to_array(two_eyes)
-                    x = np.expand_dims(x, axis=0)  # shape(1, 32, 128, 3)
+                    
+                    x = np.expand_dims(two_eyes, axis=0)  # shape(1, 32, 128, 3)
+                    x = x.astype(np.float32)  # tranfer uint8 to float32
                     x /= 255  # normalize
-                    x = np.swapaxes(x,1,2)   # swap from (1, 32, 128, 3) to (1, 128, 32, 3)
-                    self.frame_queue = np.vstack([self.frame_queue,x])
+                    self.frame_queue = np.vstack([self.frame_queue, x])
                 else:
-                    self.frame_queue = np.empty((0,128,32,3))
-
+                    self.frame_queue = np.empty((0, img_height, img_width, 3))
 
     def gaze_direction_thread(self):
         cls_list = ['LeftUp', 'Up', 'RightUp',
@@ -165,10 +162,8 @@ class Application(Frame):
         while(True):
             if self.model is not None:
                 if self.frame_queue.shape[0] >= 6:  # each 6 frame, predict a result
-                    print('There are', self.frame_queue.shape[0], 'frames')
-                    # gaze estimation
                     frames = self.frame_queue[:6]
-                    self.frame_queue = np.delete(self.frame_queue, [0,1,2,3,4,5], axis=0)
+                    self.frame_queue = np.delete(self.frame_queue, [0, 1, 2, 3, 4, 5], axis=0)
                     predicts = self.model.predict(frames)
                     predict = predicts[0] + predicts[1] + predicts[2] + predicts[3] + predicts[4] + predicts[5]
                     top_class = np.argmax(predict)
@@ -178,9 +173,9 @@ class Application(Frame):
                     self.predict['text'] = ans
                     print(predict)
                     print(ans)
+                    self.reset_nine_grid_color()
                     self.nine_grid[int(top_class / 3)][top_class % 3].configure(bg="red")
                     
-
     def on_closing(self):
         ans = askyesno(title='Quit', message='Do you want to quit?')
         if ans:
